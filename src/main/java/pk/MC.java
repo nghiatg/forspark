2aa79b3bbfea1aec776619f5f6e8b2b153c9cd6c
kjasdhvnjksDNg <tgn296@gmail.com>
2018-07-23 03:19:04 +0700

    2nd commit

--- src/main/java/pk/MC.java
+++ src/main/java/pk/MC.java
@@ -16,56 +16,61 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
 public class MC {
-	static SparkConf conf = new SparkConf().setAppName("spark-sample").set("spark.sql.parquet.binaryAsString","true");
-	static JavaSparkContext jsc = new JavaSparkContext(conf);
-	static SQLContext sqlc = new SQLContext(jsc);
-	static {
-		try {
-			jsc.setLogLevel("ERROR");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    static SparkConf conf = new SparkConf().setAppName("spark-sample").set("spark.sql.parquet.binaryAsString","true");
+    static JavaSparkContext jsc = new JavaSparkContext(conf);
+    static SQLContext sqlc = new SQLContext(jsc);
+    static {
+            try {
+                    jsc.setLogLevel("ERROR");
+            }catch(Exception e) {
+                    e.printStackTrace();
+            }
+    }
 
-	
-	// args : 0 --- log path
-	// 		  1 --- title category path
-	// 		  2 --- output path
-	public static void main(String[] args) throws Exception{
-		
-//		DataFrame banner = bannreCate().limit(1000);
-////		DataFrame parquet = readParquet(args[0]).limit(1000);
-//		DataFrame parquet = readParquet("D:\\textfiles\\parquet_banner\\parquet_logfile_at_00h_00.snap").limit(1000);
-//		parquet.join(banner,parquet.col("bannerId").equalTo(banner.col("bannerid")).and(parquet.col("geo").gt(banner.col("banner_cat")))).show();
-		DataFrame titleCate = titleCate(args[1]);
-		titleCate.show(10);
 
-	}
-	
-	public static DataFrame readParquet(String logPath) throws Exception { 
-		DataFrame df = sqlc.read().parquet(logPath);
-		return df;
-	}
-	
-	public static DataFrame bannreCate() throws Exception { 
-		BufferedReader br = new BufferedReader(new FileReader("banner_cat.txt"));
-		String line = br.readLine();
-		ArrayList<Row> rows = new ArrayList<Row>();
-		while(line != null) {
-			rows.add(RowFactory.create(Integer.parseInt(line.split("\t")[0]),Integer.parseInt(line.split("\t")[1])));
-			line = br.readLine();
-		}
-		StructType schema = new StructType(new StructField[] {
-				new StructField("bannerid",DataTypes.IntegerType,false,Metadata.empty()),
-				new StructField("banner_cat",DataTypes.IntegerType,false,Metadata.empty())
-		});
-		br.close();
-		return sqlc.createDataFrame(rows, schema);
-	}
-	
-	public static DataFrame titleCate(String titleCatePath) throws Exception {
-		DataFrame df = sqlc.read().parquet(titleCatePath);
-		return df;
-	}
+    // args : 0 --- log path
+    //                1 --- title category path
+    //                2 --- banner category path
+    //                3 --- output path
+    public static void main(String[] args) throws Exception{
+
+            DataFrame bannerCate = bannreCate(args[2]);
+            DataFrame log = readParquet(args[0]);
+            DataFrame titleCate = titleCate(args[1]);
+            //parquet.join(banner,parquet.col("bannerId").equalTo(banner.col("bannerid")).and(parquet.col("geo").gt(banner.col("banner_cat")))).show(
+            bannerCate.cache();
+            log.cache();
+            titleCate.cache();
+            DataFrame needToSave = log.join(titleCate,log.col("domain").equalTo(titleCate.col("domain")).and(log.col("path").equalTo(titleCate.col("path"))))
+                                            .join(bannerCate,log.col("bannerId").equalTo(bannerCate.col("bannerid"))).select("banner_cat","cate","click_or_view");
+            needToSave.write().format("parquet").save(args[3]);
+
+    }
+
+    public static DataFrame readParquet(String logPath) throws Exception { 
+            DataFrame df = sqlc.read().parquet(logPath);
+            return df;
+    }
+
+    public static DataFrame bannreCate(String bannerCatePath) throws Exception { 
+            BufferedReader br = new BufferedReader(new FileReader(bannerCatePath));
+            String line = br.readLine();
+            ArrayList<Row> rows = new ArrayList<Row>();
+            while(line != null) {
+                    rows.add(RowFactory.create(Integer.parseInt(line.split("\t")[0]),Integer.parseInt(line.split("\t")[1])));
+                    line = br.readLine();
+            }
+            StructType schema = new StructType(new StructField[] {
+                            new StructField("bannerid",DataTypes.IntegerType,false,Metadata.empty()),
+                            new StructField("banner_cat",DataTypes.IntegerType,false,Metadata.empty())
+            });
+            br.close();
+            return sqlc.createDataFrame(rows, schema);
+    }
+
+    public static DataFrame titleCate(String titleCatePath) throws Exception {
+            DataFrame df = sqlc.read().parquet(titleCatePath);
+            return df;
+    }
 
 }
