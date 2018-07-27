1e414c66bee57e627252959b081f0ec1591c2e4b
nghiatg <nghiatg@hadoop2395.(none)>
2018-07-27 15:35:03 +0700

    only 2

--- src/main/java/pk/MC.java
+++ src/main/java/pk/MC.java
@@ -3,7 +3,7 @@ package pk;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
@@ -33,7 +33,12 @@ public class MC {
     //                2 --- banner category path
     //                3 --- output path
     public static void main(String[] args) throws Exception{
-	joinTable(args[0],args[1],args[2],args[3]);
+	String[] paths = new String[args.length - 2];
+    	for(int i = 0 ; i < args.length - 2 ; ++i) {
+    		paths[i] = args[i];
+    	}
+    	idk(paths,Integer.parseInt(args[args.length-2]),args[args.length - 1]);
+	//joinTable(args[0],args[1],args[2],args[3]);
 	//readParquet(args[0]).show(100);
     }
 
@@ -74,5 +79,41 @@ public class MC {
             DataFrame df = sqlc.read().parquet(titleCatePath);
             return df;
     }
-
+    
+    public static void idk(String[] args, int limit,String output) throws Exception { 
+	HashMap<Integer, Integer> count = new HashMap<>();
+    	HashMap<Integer, String> match = new HashMap<>();
+    	DataFrame df = sqlc.read().parquet(args);
+    	df.registerTempTable("data");
+    	DataFrame ratio = sqlc.sql("select click.banner_cat, click.cate,  click.count as click_amount, view.count as view_amount, click.count/view.count as ratio "
+    			+ "from (select banner_cat, cate, click_or_view, count(*) as count from data "
+    			+ "where click_or_view = true and cate <> -1 and cate is not null group by banner_cat,cate,click_or_view order by banner_cat,cate) as click, "
+    			+ "(select banner_cat, cate, click_or_view, count(*) as count from data "
+    			+ "where click_or_view = false and cate <> -1 and cate is not null group by banner_cat,cate,click_or_view order by banner_cat,cate) as view "
+    			+ "where click.banner_cat = view.banner_cat and view.cate = click.cate order by ratio desc");
+    	for(Row r : ratio.collect()) {
+    		int cate = Integer.parseInt(r.getString(1).trim());
+    		if(!count.containsKey(cate)) {
+    			count.put(cate,0);
+    		}
+    		if(!match.containsKey(cate)) {
+    			match.put(cate, "");
+    		}
+    		if(count.get(cate) > limit) {
+    			continue;
+    		}
+    		count.put(cate,count.get(cate) + 1);
+    		match.put(cate, match.get(cate) + " " + r.getInt(0) + " ");
+    	}
+    	ArrayList<Row> rows = new ArrayList<Row>();
+    	for(Integer cate : count.keySet()) {
+    		rows.add(RowFactory.create(cate,match.get(cate)));
+    	}
+    	StructType schema = new StructType(new StructField[] {
+    		new StructField("cate",DataTypes.IntegerType,true,Metadata.empty()),
+    		new StructField("banner_cat",DataTypes.StringType,true,Metadata.empty())	
+    	});
+    	DataFrame tke = sqlc.createDataFrame(rows, schema);
+    	tke.write().parquet(output);
+    }
 }
